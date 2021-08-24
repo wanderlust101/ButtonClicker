8c9f1e5408720c6738e0a4c809ea8d3ef6ebea1c
wanderlust101 <76796111+wanderlust101@users.noreply.github.com>
2021-08-24 07:00:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("am I linked?")
+function logout(element) {
+	element.innerText = "Logout";
+	console.log(element)
+}
+
+function hide(element) {
+	element.remove()
+	console.log(element)
+}
